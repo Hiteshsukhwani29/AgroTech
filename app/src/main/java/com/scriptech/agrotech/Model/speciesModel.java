4de48a3fcf56360f7bcff43ea35f246a82d8e060
Saurav singh <sauravsingh85079@gmail.com>
2021-08-25 17:53:27 +0530

    Add Species Model and Adapter

--- app/src/main/java/com/scriptech/agrotech/Model/speciesModel.java
+++ app/src/main/java/com/scriptech/agrotech/Model/speciesModel.java
@@ -1,4 +1,22 @@
 package com.scriptech.agrotech.Model;
 
 public class speciesModel {
+    public String speciesName;
+
+    public  speciesModel(){
+
+    }
+
+    public speciesModel(String speciesName) {
+        this.speciesName = speciesName;
+    }
+
+    public String getSpeciesName() {
+        return speciesName;
+    }
+
+    public void setSpeciesName(String speciesName) {
+        this.speciesName = speciesName;
+    }
 }
+
