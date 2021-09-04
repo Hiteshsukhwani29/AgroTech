b1fbbf99332a051cb1da7f1c277ed965f07da408
Saurav singh <sauravsingh85079@gmail.com>
2021-09-04 15:16:04 +0530

    Add Graph page

--- app/src/main/java/com/scriptech/agrotech/Model/graphModel.java
+++ app/src/main/java/com/scriptech/agrotech/Model/graphModel.java
@@ -1,4 +1,33 @@
 package com.scriptech.agrotech.Model;
 
+import android.widget.ImageView;
+import android.widget.TextView;
+
 public class graphModel {
+    public String price;
+    public String img ;
+
+    public graphModel() {
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public graphModel(String price, String img) {
+        this.price = price;
+        this.img = img;
+    }
 }
--- app/src/main/java/com/scriptech/agrotech/graph.java
+++ app/src/main/java/com/scriptech/agrotech/graph.java
@@ -2,63 +2,86 @@ package com.scriptech.agrotech;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link graph#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.scriptech.agrotech.Adapter.SpeciesAdapter;
+import com.scriptech.agrotech.Adapter.graphAdapter;
+import com.scriptech.agrotech.Model.graphModel;
+import com.scriptech.agrotech.Model.speciesModel;
+
+import java.util.ArrayList;
+
+
 public class graph extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    RecyclerView recyclerView;
+    FirebaseFirestore db;
+    graphAdapter graphAdapter;
+    ArrayList<graphModel> graphModelArrayList;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public graph() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment graph.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static graph newInstance(String param1, String param2) {
-        graph fragment = new graph();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.frag_graph, container, false);
+
+        View v = inflater.inflate(R.layout.frag_graph, container, false);
+
+        recyclerView = v.findViewById(R.id.recycler_graph);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        db = FirebaseFirestore.getInstance();
+
+        graphModelArrayList = new ArrayList<graphModel>();
+        graphAdapter = new graphAdapter(getActivity(), graphModelArrayList);
+
+        recyclerView.setAdapter(graphAdapter);
+
+        GraphList();
+
+
+        return v;
+    }
+
+    private void GraphList() {
+        db.collection("graphList")
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+
+                        if (error != null) {
+                            Log.e("Firestore error", error.getMessage());
+                            return;
+                        }
+                        for (DocumentChange dc : value.getDocumentChanges()) {
+
+                            if (dc.getType() == DocumentChange.Type.ADDED) {
+
+                                graphModelArrayList.add(dc.getDocument().toObject(graphModel.class));
+                            }
+                            graphAdapter.notifyDataSetChanged();
+                        }
+                    }
+                });
+
     }
 }
