ef5445db4b65b11bd836ab53c0a32dac12ae79c3
Saurav singh <sauravsingh85079@gmail.com>
2021-09-05 13:35:10 +0530

    Add calculator layout

---
+++ app/src/main/java/com/scriptech/agrotech/calsi.java
@@ -0,0 +1,26 @@
+package com.scriptech.agrotech;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class calsi extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.frag_calsi, container, false);
+    }
+}
